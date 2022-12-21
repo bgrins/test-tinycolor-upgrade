2752fa3812ac0721eaed85b3979d82e082f6a832
Brian Grinstead <briangrinstead@gmail.com>
2022-12-21 14:41:23 -0800

    tinycolor 1.5.0

--- cjs/index.js
+++ cjs/index.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
-const tinycolor = require("@bgrins/test-tinycolor-esm");
+// const tinycolor = require("@bgrins/test-tinycolor-esm");
+const tinycolor = require("tinycolor2");
 
 assert(tinycolor);
 assert.equal(tinycolor("red").toHexString(), "#ff0000");
--- esm/index.js
+++ esm/index.js
@@ -1,5 +1,6 @@
 import assert from "assert";
-import tinycolor from "@bgrins/test-tinycolor-esm";
+// import tinycolor from "@bgrins/test-tinycolor-esm";
+import tinycolor from "tinycolor2";
 
 assert(tinycolor);
 assert.equal(tinycolor("red").toHexString(), "#ff0000");
